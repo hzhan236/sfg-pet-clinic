8d66e55abf3d90de2e6fa2c8f2929a56975a56af
hzhan236 <hzhan236@uw.edu>
2021-03-31 21:46:44 -0700

    adding POJOs. Closes #2.

--- src/main/java/guru/springframework/sfgpetclinic/SfgPetClinicApplication.java
+++ src/main/java/guru/springframework/sfgpetclinic/SfgPetClinicApplication.java
@@ -1,22 +1,12 @@
 package guru.springframework.sfgpetclinic;
 
-import guru.springframework.sfgpetclinic.controllers.MyController;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SfgPetClinicApplication {
 
     public static void main(String[] args) {
 
-        ApplicationContext ctx = SpringApplication.run(SfgPetClinicApplication.class, args);
-
-        MyController myController = (MyController) ctx.getBean("myController");
-
-        String greeting = myController.sayHello();
-
-        System.out.println(greeting);
     }
 
 }
